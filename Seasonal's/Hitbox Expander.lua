a25a21caaa7c3ac69a2e3b6e2855330fc9dd5b53
Proxy <99521865+SeasonalKirito@users.noreply.github.com>
2023-04-04 17:56:28 -0700

    Update Hitbox Expander.lua

--- Seasonal's/Hitbox Expander.lua
+++ Seasonal's/Hitbox Expander.lua
@@ -34,14 +34,17 @@ game:GetService('RunService').RenderStepped:connect(function()
                 end
             else
                 if v ~= lplr and v.Name ~= DeveloperUser then
-                v.Character.Head.Size = Vector3.new(settings.HeadSize,settings.HeadSize,settings.HeadSize)
-                v.Character.Head.Transparency = 1
-                v.Character.Head.CanCollide = settings.Visuals.CanCollide
-                v.Character.HumanoidRootPart.Size = Vector3.new(settings.HeadSize,settings.HeadSize,settings.HeadSize)
-                v.Character.HumanoidRootPart.Transparency = settings.Visuals.Transparency
-                v.Character.HumanoidRootPart.BrickColor = BrickColor.new(settings.Visuals.BrickColor)
-                v.Character.HumanoidRootPart.Material = settings.Visuals.Material
-                v.Character.HumanoidRootPart.CanCollide = settings.Visuals.CanCollide
+                    pcall(function()
+                        v.Character.Head.Size = Vector3.new(settings.HeadSize,settings.HeadSize,settings.HeadSize)
+                        v.Character.Head.Transparency = 1
+                        v.Character.Head.CanCollide = settings.Visuals.CanCollide
+                        v.Character.HumanoidRootPart.Size = Vector3.new(settings.HeadSize,settings.HeadSize,settings.HeadSize)
+                        v.Character.HumanoidRootPart.Transparency = settings.Visuals.Transparency
+                        v.Character.HumanoidRootPart.BrickColor = BrickColor.new(settings.Visuals.BrickColor)
+                        v.Character.HumanoidRootPart.Material = settings.Visuals.Material
+                        v.Character.HumanoidRootPart.CanCollide = settings.Visuals.CanCollide
+                    end)
+                end
             end
         end
     end
